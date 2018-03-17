d544f2377f1251261765e743545af22da5b20616
George Puisha <stearis@tut.by>
2018-03-17 19:45:25 +0300

    Add search button, search mechanism

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -21,11 +22,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     MatInputModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule,
-    BrowserAnimationsModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -10,10 +10,25 @@ import { MatInputModule } from '@angular/material/input';
 export class SearchInputComponent implements OnInit {
 
   placeholder = 'Что вы хотите выбросить?';
+  value = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public clear() {
+    this.value = '';
+  }
+
+  public search() {
+    console.log('pow');
+  }
+
+  public onKeydown(event: any) {
+    if (event.key === "Enter" || this.value.length >= 3) {
+      this.search();
+    }
   }
 
 }
