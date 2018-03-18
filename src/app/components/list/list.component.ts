90c15a3bb837678d8e0972f06663c45c3ce83414
George Puisha <stearis@tut.by>
2018-03-18 10:11:22 +0300

    Style organizations list, add test data

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -15,6 +15,26 @@ export class ListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.organizations = [
+      {
+        name: 'Итранзишн',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+        logoUrl: 'https://about.canva.com/wp-content/uploads/sites/3/2016/08/Band-Logo.png',
+        siteUrl: 'https://blog.thoughtram.io/angular/2016/10/13/two-way-data-binding-in-angular-2.html',
+        phoneOne: '+375447937743',
+        phoneTwo: '+375336679472',
+        address: 'ул.Кульман 1'
+      },
+      {
+        name: 'string',
+        description: 'string',
+        logoUrl: 'https://about.canva.com/wp-content/uploads/sites/3/2016/08/Band-Logo.png',
+        siteUrl: 'https://blog.thoughtram.io/angular/2016/10/13/two-way-data-binding-in-angular-2.html',
+        phoneOne: 'string',
+        phoneTwo: 'string',
+        address: 'string'
+      }
+    ];
   }
 
 }
